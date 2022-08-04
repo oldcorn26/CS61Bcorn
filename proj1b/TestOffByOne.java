bebc1ee659d67efc7773de20c5b4beac7a507586
Corn <304015090@qq.com>
2022-08-04 20:24:10 +0800

    proj1b 2.0

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,14 +2,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-    // You must use this CharacterComparator and not instantiate
-    // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
 
     @Test
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('&', '%'));
+        assertTrue(offByOne.equalChars('%', '&'));
         assertFalse(offByOne.equalChars('a', 'A'));
     }
 }
