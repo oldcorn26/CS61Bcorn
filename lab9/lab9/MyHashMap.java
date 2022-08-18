0666021f0a970ae771bd952332c529b7f785f9da
Corn <304015090@qq.com>
2022-08-18 17:44:23 +0800

    lab9 1.0

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -3,6 +3,9 @@ package lab9;
 import java.util.Iterator;
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
@@ -53,19 +56,47 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return buckets[hash(key)].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        int hashNum = hash(key);
+        int sizeFlag = buckets[hashNum].size();
+
+        buckets[hashNum].put(key, value);
+        if (sizeFlag != buckets[hashNum].size()) {size++;}
+
+        sizeAdjustor(buckets.length);
+    }
+
+    private void sizeAdjustor(int length) {
+        if (size > MAX_LF * length) {
+            int flag = 0;
+            K[] keys = (K[]) new Object[size];
+            V[] values = (V[]) new Object[size];
+
+            for (int i = 0; i < length; i++) {
+                for (K key : buckets[i]) {
+                    keys[flag] = key;
+                    values[flag] = buckets[i].get(key);
+                    flag++;
+                }
+            }
+
+            buckets = new ArrayMap[length * 2];
+            clear();
+            for (int i = 0; i < flag; i++) {
+                put(keys[i], values[i]);
+            }
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
