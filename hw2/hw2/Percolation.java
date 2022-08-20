adf4e06fb9e560bc60f2bceaa0c0f718de119928
Corn <304015090@qq.com>
2022-08-21 01:22:25 +0800

    HW2 1.0

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,125 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[] flag;
+    private int openNumber;
+    private static int width;
+    private WeightedQuickUnionUF sites, sites_per;
 
+    /**
+     * Create N-by-N grid, with all sites initially blocked.
+     * @param N is width of the square.
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("index " + N + " is not bigger than 0");
+        }
+
+        int num = N * N + 2;
+        width = N;
+        openNumber = 0;
+        flag = new boolean[num];
+        sites = new WeightedQuickUnionUF(num);
+        sites_per = new WeightedQuickUnionUF(num);
+
+        for (int i = 0; i < N; i++) {
+            sites_per.union(i, num - 2);
+            sites_per.union(num - 3 - i, num - 1);
+            sites.union(i, num - 2);
+        }
+    }
+
+    /**
+     * Open the site (row, col) if it is not open already.
+     * @param row is the coordinate.
+     * @param col is the coordinate.
+     */
+    public void open(int row, int col) {
+        int index = row * width + col;
+        if (row < 0 || col < 0 || row > width - 1 || col > width - 1) {
+            throw new IndexOutOfBoundsException("It's outside its prescribed range");
+        }
+
+        flag[index] = true;
+        if (col < width - 1 && flag[index + 1]) {
+            sites.union(index, index + 1);
+            sites_per.union(index, index + 1);
+        }
+        if (row < width - 1) {
+            if (flag[index + width]) {
+                sites.union(index, index + width);
+                sites_per.union(index, index + width);
+            }
+        }
+        if (row > 0) {
+            if (flag[index - width]) {
+                sites.union(index, index - width);
+                sites_per.union(index, index - width);
+            }
+        }
+        if (col > 0) {
+            if (flag[index - 1]) {
+                sites.union(index, index - 1);
+                sites_per.union(index, index - 1);
+            }
+        }
+
+        openNumber++;
+    }
+
+    /**
+     * Ts the site (row, col) open?
+     * @param row is the coordinate.
+     * @param col is the coordinate.
+     * @return true if it is open.
+     */
+    public boolean isOpen(int row, int col) {
+        int index = row * width + col;
+        if (row < 0 || col < 0 || row > width - 1 || col > width - 1) {
+            throw new IndexOutOfBoundsException("Outside its prescribed range");
+        }
+        return flag[index];
+    }
+
+    /**
+     * Is the site (row, col) full?
+     * @param row is the coordinate.
+     * @param col is the coordinate.
+     * @return true if it is full.
+     */
+    public boolean isFull(int row, int col) {
+        int index = row * width + col;
+        if (row < 0 || col < 0 || row > width - 1 || col > width - 1) {
+            throw new IndexOutOfBoundsException("Outside its prescribed range");
+        }
+        return sites.connected(index, width * width - 2);
+    }
+
+    /**
+     * Number of open sites
+     * @return the number of open sites
+     */
+    public int numberOfOpenSites() {
+        return openNumber;
+    }
+
+    /**
+     * Does the system percolate?
+     * @return true if it is percolated.
+     */
+    public boolean percolates() {
+        int num  = width * width;
+        return sites_per.connected(num, num + 1);
+    }
+//    public static void main(String[] args) {
+//        Percolation p = new Percolation(3);
+//        p.open(0,0);
+//        p.open(1,0);
+//        p.open(2,0);
+//        p.open(1,2);
+//        p.open(2,2);
+//        System.out.println(p.numberOfOpenSites());
+//        System.out.println(p.isOpen(1, 2));
+//        System.out.println(p.isFull(1, 2));
+//    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,65 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+
+public class PercolationStats {
+    private double[] fractions;
+    private static int times;
+
+    /**
+     * Perform T independent experiments on an N-by-N grid
+     * @param N is the width.
+     * @param T is the experiment times.
+     * @param pf is the PercolationFactory
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("The input is not allowed");
+        }
+
+        times = T;
+        fractions = new double[T];
+        Percolation p = pf.make(N);
+        int sitesNumber = N * N;
+        for (int i = 0; i < T; i++) {
+            while (!p.percolates()) {
+                p.open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+            fractions[i] = p.numberOfOpenSites() / sitesNumber;
+        }
+    }
+
+    /**
+     * Sample mean of percolation threshold
+     * @return the mean of experiment.
+     */
+    public double mean(){
+        return StdStats.mean(fractions);
+    }
+
+    /**
+     * Sample standard deviation of percolation threshold
+     * @return the sample standard deviation of percolation threshold
+     */
+    public double stddev(){
+        return StdStats.stddev(fractions);
+    }
+
+    /**
+     * Calculate low endpoint of 95% confidence interval
+     * @return the low endpoint of 95% confidence interval
+     */
+    public double confidenceLow(){
+        return mean() - 1.96 * stddev() / Math.sqrt(times);
+    }
+
+    /**
+     * Calculate high endpoint of 95% confidence interval
+     * @return the high endpoint of 95% confidence interval
+     */
+    public double confidenceHigh(){
+        return mean() + 1.96 * stddev() / Math.sqrt(times);
+    }
 }
