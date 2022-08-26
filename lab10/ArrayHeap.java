572bb7f0488085dfd0699652e89f6a4bb634baaf
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2022-08-26 22:01:07 +0800

    Add files via upload
    lab10 2.0

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -210,9 +210,13 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     public void changePriority(T item, double priority) {
         for (int i = 1; i <= size; i++) {
             if (contents[i].myItem.equals(item)) {
-                contents[i].myPriority = priority;
-                swim(i);
-                sink(i);
+                if (contents[i].myPriority > priority) {
+                    contents[i].myPriority = priority;
+                    swim(i);
+                } else {
+                    contents[i].myPriority = priority;
+                    sink(i);
+                }
                 return;
             }
         }
